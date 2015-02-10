df138bc99d7b4bb1ef7bf3939f059c46aa9686ab
oliver <@>
2015-02-11 00:25:08 +0100

    add mScale()

--- includes/math/math3D.js
+++ includes/math/math3D.js
@@ -10,13 +10,25 @@ Math3D.mRotation = function (vAxis, nRotateRadians) {
 };
 
 
+Math3D.mScale = function (vS) {
+	
+	return Float32Array([
+		vS[0],0,0,0,
+		0,vS[1],0,0,
+		0,0,vS[2],0,
+		0,0,0,vS[3],
+	]);
+	
+};
+
+
 Math3D.mTranslation = function (vT) {
 	
 	return Float32Array([
 		1,0,0,0,
 		0,1,0,0,
 		0,0,1,0,
-		vT[0],vT[1],vT[2],1,
+		vT[0],vT[1],vT[2],vT[3],
 	]);
 	
 };
