9a4c8cfc55c1745b4302b0c2d756eea2e68eb353
Oliver Osterloh <osterloh@source-lounge.de>
2015-02-28 19:42:27 +0100

    Math3D: add nLength, pAdd, pSub

--- includes/math/Math3D/points.js
+++ includes/math/Math3D/points.js
@@ -5,6 +5,12 @@ var Math3D = Math3D || {};
 
 
 
+/**
+ * scalation
+ * axisX = pPxP(axisY, axisZ)
+ * axisX = pPxP(axisY, axisZ)
+ * axisX = pPxP(axisY, axisZ)
+ */
 Math3D.pPxP = function (pA, pB) {
 	
 	var pReturn = new Float32Array(3);
@@ -24,13 +30,48 @@ Math3D.pNormalize = function (pP) {
 	
 	var pReturn = new Float32Array(3);
 	
-	var nLength = Math.sqrt(pP[0] * pP[0] + pP[1] * pP[1] + pP[2] * pP[2]);
+	var nLength = Math3D.nLength(pP);
 	
 	pReturn[0] = pP[0] / nLength;
 	pReturn[1] = pP[1] / nLength;
 	pReturn[2] = pP[2] / nLength;
 	
 	return pReturn;
+	
+};
+
+
+
+
+Math3D.nLength = function (pP) {
+	
+	return Math.sqrt(pP[0] * pP[0] + pP[1] * pP[1] + pP[2] * pP[2]);
+	
+};
+
+
+
+
+Math3D.pAdd = function (pA, pB) {
+	
+	return new Float32Array([
+		pA[0] + pB[0],
+		pA[1] + pB[1],
+		pA[2] + pB[2],
+	]);
+	
+};
+
+
+
+
+Math3D.pSub = function (pA, pB) {
+	
+	return new Float32Array([
+		pA[0] - pB[0],
+		pA[1] - pB[1],
+		pA[2] - pB[2],
+	]);
 	
 };
 
