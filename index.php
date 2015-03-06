eb853f0be6ed70b97f5a2538ba6bfd5a09823a6c
Oliver Osterloh <osterloh@source-lounge.de>
2015-03-06 17:53:03 +0100

    add game structure, change test structure

--- index.php
+++ index.php
@@ -27,11 +27,14 @@
 			$aFilesInInclude = aListFiles('includes');
 			
 			$aIncludeFiles = array();
+			$aGameFiles = array();
 			$aTestFiles = array();
 			
 			foreach ($aFilesInInclude as $sFileInInclude) {
-				if (preg_match('/\.js$/', $sFileInInclude)) {
-					if (preg_match('/\/test\//', $sFileInInclude)) {
+				if (preg_match('#\.js$#', $sFileInInclude)) {
+					if (preg_match('#/games/#', $sFileInInclude)) {
+						$aGameFiles []= $sFileInInclude;
+					} else if (preg_match('#/tests/#', $sFileInInclude)) {
 						$aTestFiles []= $sFileInInclude;
 					} else {
 						$aIncludeFiles []= $sFileInInclude;
@@ -39,7 +42,19 @@
 				}
 			}
 			
-			$sMainFile = 'main.js';
+			$sMainFile = '';
+			if (isset($_REQUEST['game'])) {
+				$sGame = $_REQUEST['game'];
+				foreach ($aGameFiles as $sGameFile) {
+					if (preg_match('#/games/' . preg_quote($sGame) . '/#', $sGameFile)) {
+						if (preg_match('#/main\.js$#', $sGameFile)) {
+							$sMainFile = $sGameFile;
+						} else {
+							$aIncludeFiles []= $sGameFile;
+						}
+					}
+				}
+			}
 			if (isset($_REQUEST['test'])) {
 				$sTest = $_REQUEST['test'];
 				foreach ($aTestFiles as $sTestFile) {
